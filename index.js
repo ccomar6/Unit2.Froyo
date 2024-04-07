b1c7f6af45af05ed2c58dc848e2f103227c3a66d
Carson Comar <Carsonccomar@gmail.com>
2024-04-07 02:22:52 -0500

    hopefully second to last update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //They type vanilla,vanilla,vanilla,strawberry,coffee,coffee. When they view the browser console, they observe a table listing how many of 
 //each flavor they have ordered. In this case, they will be able to observe that they have ordered three vanilla, two coffee, and one strawberry froyo.
 
-alert(dfdf)
+alert('dfdf')
 const string = 'vanilla, vanilla, vanilla, strawberry, coffee, coffee'
 
 const myObj = {}
